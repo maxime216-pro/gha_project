e992f518a61f53da393c6022fbfb196ebe14c292
DessezPro <dessez.pro@gmail.com>
2020-10-31 11:54:55 +0100

    Tweak some configs Add a few more dependencies Reorganize folder structure

---
+++ src/Infrastructure/Controller/GHA/CommitComments.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Infrastructure\Controller\GHA;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+final class CommitComments extends AbstractController
+{
+    public function __invoke(Request $request)
+    {
+        
+    }
+}
